9b3d26608cbe49d5afdce5ce1ebf349811e36ae8
BasHofland <33237@ma-web.nl>
2021-09-17 10:09:11 +0200

    changes

---
+++ name.py
@@ -0,0 +1,3 @@
+Naam = input("Wat is jouw naam? ")
+print("Dit is " + Naam + "'s code!")
+print("Im sorry to inform you " + Naam + ", but your mom gay")
---
+++ test.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+
+Getal1 = 5+5
+Getal2 = 5-5
+Getal3 = 5*5
+Getal4 = 5/5
+
+print(Getal1, Getal2, Getal3, Getal4)
