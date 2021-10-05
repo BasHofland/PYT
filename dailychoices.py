323d82f9a98c103f25807b55a14b94c0ffb47dd2
Bas Hofland <33237@ma-web.nl>
2021-10-05 12:27:31 +0200

    Dailychoice

---
+++ dailychoices.py
@@ -0,0 +1,34 @@
+print("Ben je bang in het donker?")
+keuze1 = input()
+if keuze1 == "ja":
+    print("Jammer")
+else:
+    print("Mooizo")
+
+print("Ben je blij met je naam?")
+keuze2 = input()
+if keuze2 == "ja":
+    print("Mooizo")
+else:
+    print("Jammer")
+
+print("Ben je een avondmens?")
+keuze3 = input()
+if keuze3 == "ja":
+    print("Cool")
+else:
+    print("Cool")
+
+print("Ben je een optimist?")
+keuze4 = input()
+if keuze4 == "ja":
+    print("Mooizo")
+else:
+    print("Jammer")
+
+print("Ben je egoïstisch?")
+keuze5 = input()
+if keuze5 == "ja":
+    print("Homo")
+else:
+    print("Mooi")
